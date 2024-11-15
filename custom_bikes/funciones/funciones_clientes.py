e229028da41cbd368f138dd7ec64cf69ab873b18
Endert Guerrero <alejandroguerrero200404@gmail.com>
2024-11-15 12:13:20 -0300

    funcion agregar_cliente_csv

--- custom_bikes/funciones/funciones_clientes.py
+++ custom_bikes/funciones/funciones_clientes.py
@@ -31,6 +31,54 @@ def insert_cliente():
     finally:
         conn.close()
 
+def agregar_cliente_csv():
+    import csv
+
+    conn = sqlite3.connect('custom_bikes/custom_bikes.db')
+    cursor = conn.cursor()
+
+    with open('custom_bikes\datos\personas_respaldo.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  
+
+        for row in reader:
+            rut_id = row[0]
+            nombre = row[1]
+            apellido = row[2]
+            telefono = row[3]
+            correo = row[4]
+            direccion = row[5]
+
+            try:
+                cursor.execute('''INSERT INTO personas (rut_id, nombre, apellido, telefono, correo, direccion)
+                                  VALUES (?, ?, ?, ?, ?, ?)''', (rut_id, nombre, apellido, telefono, correo, direccion))
+
+                conn.commit()
+                print(f'Cliente {nombre} {apellido} agregado exitosamente.')
+
+            except sqlite3.Error as e:
+                print("Error al insertar el cliente:", e)
+
+    with open('custom_bikes\datos\cliente_respaldo.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+
+        for row in reader:
+            rut_id = row[0]
+            altura = row[1]
+
+            try:
+                cursor.execute('''INSERT INTO cliente (rut_id, altura)
+                                  VALUES (?, ?)''', (rut_id, altura))
+
+                conn.commit()
+                print(f'Cliente {rut_id} agregado exitosamente.')
+
+            except sqlite3.Error as e:
+                print("Error al insertar el cliente:", e)
+    
+        conn.close()
+
 def seleccionar_cliente():
     conn = sqlite3.connect('custom_bikes/custom_bikes.db')
     cursor = conn.cursor()
@@ -52,6 +100,4 @@ def seleccionar_cliente():
         return cliente
     else:
         print("Cliente no encontrado.")
-        return None
-
-seleccionar_cliente()
+        return None  
